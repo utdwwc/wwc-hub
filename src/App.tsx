91dae75f8c0698dc3e479c45df10ad8f56891164
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2023-06-19 13:15:34 -0500

    update project settings

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar';
+import Header from './header';
+import Values from './Values';
+
+function App() {
+  return (
+    <div className="App">
+      < Navbar />
+      < Header />
+      <Values />
+    </div>
+  );
+}
+
+export default App;

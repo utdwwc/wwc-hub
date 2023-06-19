292b29179ffcc285eb48d5aac9a46caeddaf3a9a
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2023-06-19 13:15:16 -0500

    create values component

---
+++ src/Values.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './css/values.module.css';
+
+function Values() {
+    return (
+        <>
+        <div className={styles.valuesContainer}>
+            <div className={styles.heading} >
+                What We Offer
+            </div>
+            <div className={styles.subHeading} >
+                Our services are centered around mentorship, personal and professional growth.
+            </div>
+            <img src='valuesBack.svg' alt='valuesVector' className={styles.vector} />
+        </div>
+        
+    </>
+    );
+  }
+  
+  export default Values;
+  

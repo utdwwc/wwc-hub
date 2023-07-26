5949e8a04be0d3f7e8c25951e3ad7d8c3da3c466
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2023-07-26 08:01:11 -0500

    create partners component

---
+++ src/Partners.tsx
@@ -0,0 +1,10 @@
+import styles from './css/partner.module.css';
+
+function Partners() {
+    return (
+        <img className={styles.partner} src='partnerPic.svg' alt='partners' />
+    );
+  }
+  
+  export default Partners;
+  

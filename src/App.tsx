bef177ba8068acacf9cd0da0e10258a61f30b2fb
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2023-08-25 20:07:49 -0500

    update packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react';
 import Navbar from './sections/Navbar';
 import Header from './sections/Header';
 import Values from './sections/Values';
@@ -7,18 +6,49 @@ import Events from './sections/Events';
 import Partners from './sections/Partners';
 import Footer from './sections/Footer';
 import Team from './sections/Team';
+import { useState } from 'react';
 
-function App() {
+  const App = () => {
+    const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e: { clientX: any; clientY: any; }) => {
+    setCursorPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  useEffect(() => {
+    const cursor = document.querySelector('.custom-cursor') as HTMLElement;
+
+    if (cursor) {
+      cursor.style.left = `${cursorPosition.x}px`;
+      cursor.style.top = `${cursorPosition.y}px`;
+    }
+  }, [cursorPosition]);
+
+  
   return (
-    <div className="App">
-      < Navbar />
-      < Header />
-      < Values />
-      < Events />
-      < Team />
-      < Partners />
-      < Footer />
+    <>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <div className="App" onMouseMove={handleMouseMove}>
+    <div className="custom-cursor"></div>
+        < Navbar />
+        <div id="header">
+          < Header />
+        </div>
+        <div id="values">
+          < Values />
+        </div>
+        <div id="events">
+          < Events />
+        </div>
+        <div id="officers">
+          < Team />
+        </div>
+        <div id="partners">
+          < Partners />
+        </div>
+        < Footer />
     </div>
+    </>
   );
 }
 

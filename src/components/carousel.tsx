7d8a4d275fd1af2562fe6ba1d2e9a0d025430e14
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2023-12-30 12:16:36 -0600

    card carousel remove arrows

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -4,6 +4,8 @@ import styles from '../css/carousel.module.css';
 import leftArrow from '../images/leftarrow.svg';
 import rightArrow from '../images/rightarrow.svg';
 
+
+
 interface CardProps {
   title: string;
   date: string;
@@ -29,20 +31,15 @@ const Carousel: React.FC<CarouselProps> = ({ cards }) => {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + cards.length) % cards.length);
   };
 
-
   return (
     <>
-    <div className={styles.arrows}>
+    <div className={styles.spacing} ></div>
+    {/* <div className={styles.arrows}>
         <img src={leftArrow} className={styles.leftArrow} onClick={goToPrevSlide} alt='leftArrow' />
         <img src={rightArrow} className={styles.rightArrow} onClick={goToNextSlide} alt='rightArrow' />
-    </div>
-    <div className={styles.carousel}>
-      <div className={styles.carouselContainer}>
-        <div
-          className={styles.carouselSlide}
-          style={{ transform: `translateX(-${currentIndex * 33}%)` }}
-        >
-          {cards.map((card, index) => (
+    </div> */}
+    <div className={styles.rowContain} >
+    {cards.map((card, index) => (
             <div key={index} className={styles.card}>
               <Card
                 title={card.title}
@@ -55,11 +52,51 @@ const Carousel: React.FC<CarouselProps> = ({ cards }) => {
               />
             </div>
           ))}
-        </div>
-      </div>
     </div>
-    </>
+    </> 
+    
   );
 };
 
 export default Carousel;
+
+
+
+// first step: card is done- comment out all the other cards except one and display so that it displays
+// make all the card components show in one single row 
+//make the arrows functional to sort through the cards
+
+
+
+//old: 
+
+// return (
+  //   <>
+  //   <div className={styles.arrows}>
+  //       <img src={leftArrow} className={styles.leftArrow} onClick={goToPrevSlide} alt='leftArrow' />
+  //       <img src={rightArrow} className={styles.rightArrow} onClick={goToNextSlide} alt='rightArrow' />
+  //   </div>
+  //   <div className={styles.carousel}>
+  //     <div className={styles.carouselContainer}>
+  //       <div
+  //         className={styles.carouselSlide}
+  //         style={{ transform: `translateX(-${currentIndex * 33}%)` }}
+  //       >
+  //         {cards.map((card, index) => (
+  //           <div key={index} className={styles.card}>
+  //             <Card
+  //               title={card.title}
+  //               date={card.date}
+  //               subtitle={card.subtitle}
+  //               picture={card.picture}
+  //               startTime={card.startTime}
+  //               endTime={card.endTime}
+  //               location={card.location}
+  //             />
+  //           </div>
+  //         ))}
+  //       </div>
+  //      </div>
+  //    </div>
+  //   </> 
+  // );
